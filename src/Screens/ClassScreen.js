7c65b19d88efdaffb2ebfb8eebf4dc9aa03d7284
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-04-24 01:48:55 +0530

    Revert "integrating sufi and amans ui for presentation"

---
+++ src/Screens/ClassScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CourseStream from '../Components/CourseStream'
+import NavbarCustom from "../Components/NavbarCustom"
+const ClassScreen = () => {
+    return (
+        <div>
+            <NavbarCustom/>
+            <CourseStream/>
+        </div>
+    )
+}
+
+export default ClassScreen
---
+++ src/Screens/Homescreen.js
@@ -0,0 +1,13 @@
+import { Paper } from '@material-ui/core'
+import React from 'react'
+import Card from "../Components/CourseCard"
+const Homescreen = () => {
+    return (
+        <Paper>
+            <Card/>
+            <Card/>
+        </Paper>
+    )
+}
+
+export default Homescreen
